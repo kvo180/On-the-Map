ece34a06b006bec2df0b3e26dd0e835f2ed7b707
kvo180 <khoavo180@gmail.com>
2015-11-12 03:00:35 -0800

    "Sign Up" button on Login view opens signup link in Safari
    -Add placeholder text for login textfields
    -Add textfield delegate methods and tap gesture to dismiss keyboard

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -14,25 +14,75 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailLoginTextField: UITextField!
     @IBOutlet weak var passwordLoginTextField: UITextField!
+    let emailPlaceholderText = "Email"
+    let passwordPlaceholderText = "Password"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        emailLoginTextField.delegate = self
+        passwordLoginTextField.delegate = self
         
+        emailLoginTextField.text = emailPlaceholderText
+        passwordLoginTextField.text = passwordPlaceholderText
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
+
+    // MARK: - IBActions
 
     @IBAction func loginButtonPressed(sender: AnyObject) {
         print("logged in")
     }
     
     @IBAction func signUpButtonPressed(sender: AnyObject) {
-        print("signed up")
+        
+        let signUpPageURL = NSURL(string: "https://www.udacity.com/account/auth#!/signup")
+        UIApplication.sharedApplication().openURL(signUpPageURL!)
+
     }
     
-
     @IBAction func facebookSignInButtonPressed(sender: AnyObject) {
+    }
+    
+    // MARK: - Utilities
+    // Close keyboard whenever user taps anywhere outside of keyboard:
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
 
 }
 
+// MARK: - UITextField Delegate Methods
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+        // If placeholder text is displayed, empty textfield when beginning editing
+        if textField == emailLoginTextField && textField.text == emailPlaceholderText {
+            textField.text = ""
+        }
+        else if textField == passwordLoginTextField && textField.text == passwordPlaceholderText {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+        // If textField is empty, show respective default placeholder text
+        if textField.text == "" {
+            if textField == emailLoginTextField {
+                textField.text = emailPlaceholderText
+            }
+            else {
+                textField.text = passwordPlaceholderText
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
